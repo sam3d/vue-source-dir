979361fb42ededabd5496d26f748466f2624bf3e
Sam Holmes <samholmes1337@gmail.com>
2019-10-31 11:50:39 +0000

    Tidy up resolver

--- index.js
+++ index.js
@@ -2,25 +2,39 @@ const _ = require("lodash");
 const path = require("path");
 
 module.exports = function handler(api, options) {
-  let sourceDir = _.get(options, "pluginOptions.sourceDir"); // Get the plugin configuration
-  let entryFile = _.get(options, "pluginOptions.entryFile", `main.js`); // Get the entry file if it is set, default to main.js otherwise
-  let entryFilePath = `./${sourceDir}/${entryFile}`; // Combine the source directory and entry file
-  if (!sourceDir) return; // If there's no source dir option, don't do anything
-  sourceDir = path.relative(api.service.pkgContext, sourceDir); // Parse the source dir relative to the vue project root
-  setup(api, sourceDir, entryFilePath); // Start the plugin setup process
+  let sourceDir = _.get(options, "pluginOptions.sourceDir");
+  let entryFile = _.get(options, "pluginOptions.entryFile", `main.js`);
+
+  // If there's no source dir option, don't do anything. This is required for
+  // operation of this plugin.
+  if (!sourceDir) return;
+
+  /**
+   * Resolve a file path relative to the root of the vue project.
+   * @param {string} file The file path to resolve.
+   */
+  const fromRoot = file => path.relative(api.service.pkgContext, file);
+
+  // Resolve the relative file paths.
+  sourceDir = fromRoot(sourceDir);
+  entryFile = fromRoot(path.resolve(sourceDir, entryFile));
+
+  // Start the plugin setup process.
+  setup(api, sourceDir, entryFile);
 };
 
-function setup(api, sourceDir, entryFilePath) {
-  api.chainWebpack(config => setupWebpack(config, sourceDir, entryFilePath)); // Configure all webpack options
+function setup(api, sourceDir, entryFile) {
+  // Configure all webpack options.
+  api.chainWebpack(config => setupWebpack(config, sourceDir, entryFile));
 }
 
-function setupWebpack(config, sourceDir, entryFilePath) {
+function setupWebpack(config, sourceDir, entryFile) {
   /**
    * Configure the new entry point.
    */
   const entry = config.entry("app");
   entry.clear(); // Remove existing entry rule
-  entry.add(entryFilePath);
+  entry.add(entryFile);
 
   /**
    * Configure how the @ symbol is resolved.
